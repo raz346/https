15cd1f30cc6746814d37980776d7fe72c1998ae4
raz346 <alhashimi346@gmail.com>
2019-08-28 00:18:08 +0000

    print data in chunks

--- step1.js
+++ step1.js
@@ -1,15 +1,16 @@
 var https = require('https');
-
 function getAndPrintHTMLChunks () {
-
+  var dataReceived = '';
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
   https.get(requestOptions, function(response) {
     response.setEncoding('utf8');
-    response.on('data', function(){
-      console.log('your data is '+ data)
+    response.on('data', function(data){
+      dataReceived += data + '\n';
+      console.log('your data is '+ dataReceived)
     })
   });
 }
+getAndPrintHTMLChunks();
